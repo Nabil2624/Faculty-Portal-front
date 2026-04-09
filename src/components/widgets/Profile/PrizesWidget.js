94a3ad03e31e70118d4c28e08f7996d3ab29f2ea
Nabil2624 <nabilalzohari@gmail.com>
2026-04-09 15:20:43 +0200

    blur cv template in profile dashboard and recolor the icons

--- src/components/widgets/Profile/PrizesWidget.js
+++ src/components/widgets/Profile/PrizesWidget.js
@@ -8,12 +8,12 @@ export default function PrizesWidget({ count = 0, isArabic }) {
     <div className="flex flex-col items-center justify-center w-full h-full p-[clamp(12px,1vw,20px)] text-center bg-white border border-gray-100 rounded-[clamp(14px,1vw,20px)] shadow-inner">
       {/* Title + Icon Wrapper */}
       <div className="flex items-center gap-[clamp(6px,1vw,12px)] mb-[clamp(4px,0.5vw,10px)]">
-        <div className="bg-[#19355A] p-1.5 rounded-lg shadow-sm">
+        <div className="bg-[#fff] p-1.5 rounded-lg shadow-sm border-1 border-[#19355A]">
           <Award
-            className="text-[#b38e19]"
+            className="text-[#19355A]"
             style={{
-              width: "clamp(16px,1.2vw,24px)",
-              height: "clamp(16px,1.2vw,24px)",
+              width: "clamp(25px,1.2vw,24px)",
+              height: "clamp(25px,1.2vw,24px)",
             }}
           />
         </div>
