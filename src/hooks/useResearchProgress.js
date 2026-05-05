14f0726a3cda031b7551232ad8ebbc16de8dc450
Ahmed Ehab <135270188+AhmedEhab5006@users.noreply.github.com>
2026-05-05 16:47:06 +0300

    ...

--- src/hooks/useResearchProgress.js
+++ src/hooks/useResearchProgress.js
@@ -16,7 +16,7 @@ const useResearchProgress = (nationalNumber) => {
 
     // إعداد الاتصال
     const connection = new signalR.HubConnectionBuilder()
-      .withUrl("https://172.1.50.98/hubs/researchProgressHub")
+      .withUrl("https://172.1.50.98/core/hubs/researchProgressHub")
       .withAutomaticReconnect()
       .build();
 
