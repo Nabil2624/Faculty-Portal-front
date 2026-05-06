752d3c6693aec3786ea4203f54b99771701507e8
Ahmed Ehab <135270188+AhmedEhab5006@users.noreply.github.com>
2026-05-06 13:35:19 +0300

    Testing ns

--- src/i18n.js
+++ src/i18n.js
@@ -13,9 +13,34 @@ i18n
     supportedLngs: ["ar", "en"],
 
     // Namespaces (optional — keep simple if you use one file per page)
-    ns: [""],
-    defaultNS: "",
+    ns: [
+      "login", "dashboard", "HeaderAndSideBar", "PersonalData",
+      "CV", "Chat", "OTP", "error", "form", "filter-sort",
+      "forgetpassword", "register", "identification", "contactinfo",
+      "ScientificResearches", "ScientificResearchDetails", "ScientificResearchFullDetails",
+      "AddScientificResearch", "NominatedResearch", "ResearcherProfile",
+      "AcademicQualifications", "add-academic-qualification",
+      "AdministrativePositions", "GeneralExperiences", "add-general-experiences",
+      "JobRanks", "Theses", "ThesesDetails", "AddThesis", "EditThesis",
+      "SupervisionThesis", "SupervisionInfo", "AddSupervision", "RecommendedSupervisions",
+      "AddProject", "projects", "AddArticleForm", "ParticipationJournals",
+      "SeminarsAndConferences", "add-conference", "CommitteesAssociations",
+      "add-committee", "TrainingPrograms", "add-training-program",
+      "ScientificMissions", "add-scientific-task", "patents", "patent-form",
+      "prizes-and-rewards", "prizes-and-rewards-form", "scientific-writing",
+      "scientific-writing-form", "community-service", "participation-quality-work",
+      "participation-quality-work-form", "journal-forms", "article-reviews",
+      "teaching-experiences", "teaching-experience-form", "socialNetworkingPages",
+      "google-scholer-popUp", "CitationsChart", "QuotesChart", "Logs",
+      "LogsCategories", "Users", "AdminFacultyData", "admin-dashboard",
+      "university-contribution", "university-contribution-form",
+      "manifestations-of-scientific-appreciation", "manifestation-of-scientific-appreciation-form",
+      "AddProject", "UnderDevelopment", "ResetPassword", "Ticketing",
+      "session-popup", "unauthorizedModal", "HeaderAndSideBar"
+    ],
+    defaultNS: "login",
 
+    
     backend: {
       loadPath: "/locales/{{lng}}/{{ns}}.json", // Path to translation files
     },
